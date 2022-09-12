1849d6678fbf4843d7c5bf9a7b2f05a2c17f1c64
BillerJully <billerjully@gmail.com>
2022-09-12 11:14:58 +0300

    new file

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var num = 10;
 console.log(num);
 var num_2 = 10;
 console.log(num_2);
+var author = "Hillo";
+console.log(author);
--- index.ts
+++ index.ts
@@ -3,3 +3,7 @@ var num: number = 10;
 console.log(num);
 var num_2: number = 10;
 console.log(num_2);
+
+var author: string = "Hillo";
+
+console.log(author);
